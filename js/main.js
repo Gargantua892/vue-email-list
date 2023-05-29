c936c75ee4369e99c094172b1946667abb58077a
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-05-29 15:22:00 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            mail: 0,
+            mail: [
+
+        ],
+
             
         }
     },
@@ -13,8 +16,10 @@ createApp({
         for(let i = 0; i < 10; i++){
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (response) {
-                console.log(response.data);
+            .then(
+                apiMail => {
+                    console.log(apiMail.data);
+                    this.mail.push({ user: apiMail.data.response });
             });
         }
     },
